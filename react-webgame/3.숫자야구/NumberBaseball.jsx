fc3d0d57b2f9752b49b8edf317a9d661b3d9c629
LouiMinister <LouiMinister@gmail.com>
2020-01-17 22:39:14 +0900

    2020-10-18 3-5: 주석과 메서드바인딩

--- react-webgame/3.숫자야구/NumberBaseball.jsx
+++ react-webgame/3.숫자야구/NumberBaseball.jsx
@@ -41,7 +41,7 @@ class NumberBaseball extends Component {
                 <ul>
                     {this.fruits.map( (v, i) => {
                         return (
-                            <Try value={v} index={i}/>
+                            <Try key={v.fruit + v.taste} value={v} index={i}/>
                         );
                     })}
                 </ul>
--- react-webgame/3.숫자야구/Try.jsx
+++ react-webgame/3.숫자야구/Try.jsx
@@ -3,7 +3,7 @@ import React, {Component } from 'react';
 class Try extends Component {
     render() {
         return (
-            <li key = {this.props.value.fruit + this.props.value.taste}>
+            <li>
                 <div>{this.props.index}. {this.props.value.fruit} - {this.props.value.taste}</div>
                 <div>컨텐츠</div>
                 <div>컨텐츠</div>
