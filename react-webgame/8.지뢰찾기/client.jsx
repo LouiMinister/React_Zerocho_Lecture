8f76e3faa546de205b60fae93b476cdd707d3e4f
LouiMinister <LouiMinister@gmail.com>
2020-02-18 20:39:15 +0900

    Init 지뢰찾기 React Project
    Add npm
    Add packages
    Add index.html
    Add client.jsx

---
+++ react-webgame/8.지뢰찾기/client.jsx
@@ -0,0 +1,12 @@
+
+import TicTacToe from "./TicTacToe";
+
+const React = require('react');
+const ReactDom = require('react-dom');
+const { hot } = require('react-hot-loader/root');
+
+//const ResponseCheck  = require('./ResponseCheck');
+
+const Hot = hot(TicTacToe);
+
+ReactDom.render(<Hot />, document.querySelector('#root'));
